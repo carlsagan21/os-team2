404de7c98d52116d5445fa39b96e7677bdb17913
soo <carlsagan96@gmail.com>
2017-04-09 18:50:56 +0900

    silence useless headers

--- arch/arm/kernel/rotunlock_read.c
+++ arch/arm/kernel/rotunlock_read.c
@@ -1,11 +1,11 @@
-#include <linux/unistd.h>
+// #include <linux/unistd.h>
 #include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/list.h>
-#include <linux/cred.h> /* for task_uid() */
-#include <linux/slab.h> /* for kmalloc(), GFP_KERNEL. linux/gfp 따로 include 안해도 됨. */
-#include <linux/uaccess.h> /* for copy_from_user, copy_to_user, strncpy */
-#include <linux/errno.h>
+// #include <linux/sched.h>
+// #include <linux/list.h>
+// #include <linux/cred.h> /* for task_uid() */
+// #include <linux/slab.h> /* for kmalloc(), GFP_KERNEL. linux/gfp 따로 include 안해도 됨. */
+// #include <linux/uaccess.h> /* for copy_from_user, copy_to_user, strncpy */
+// #include <linux/errno.h>
 
 /*
  * Release a read/or write lock using the given rotation range
