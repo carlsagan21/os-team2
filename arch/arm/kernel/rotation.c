d808c6cc6672201ba75f4d79a05a894137bbe8eb
woong <wooong1225@gmail.com>
2017-04-12 21:31:18 +0900

    Implement wl_acquire function

--- arch/arm/kernel/rotation.c
+++ arch/arm/kernel/rotation.c
@@ -62,9 +62,56 @@ int waiting_list_refresh(void) { // reconstruct pending_lh, wait_read_lh, wait_w
   // acquire 는 안돈다
   return 0;
 };
-// int wl_acquire(void) {
-// return 0;
-// };
+
+
+int waitingLockOverlapAcquired(rotlock_t *waiting_lock,acquired_lock){
+  /*
+   * Check whether degree/range of waiting_lock_node overlaps with that of acquired_lock
+   * if not overlapped -> 0 , else -> 1
+  */
+  return 0;
+};
+
+void waitingLockAdjustLink(rotlock_t *waiting_lock){
+  /*
+   * Remove target(waiting_lock) from wait_write_lh
+   * You have to adjust link from prev and next node of waiting_lock
+   * A <-> waiting_lock <-> C
+   * Adjust next of A point to C. Adjust prev of C point to A
+  */
+  return;
+};
+
+void addLockToAcquiredLock(rotlock_t *waiting_lock){
+  /*
+   * Add target waiting_lock to acquired_lock_list
+   * Regarding Queue structure of acquired_lock_list, enQueue(waiting_lock);
+   *
+  */
+  return;
+};
+
+//move waiting process list element to acquired process list
+ int wl_acquire(void) {
+   rotlock_t *waiting_lock;
+   rotlock_t *waiting_safe_lock;
+   rotlock_t *acquired_lock;
+   rotlock_t *acquired_safe_lock;
+
+   list_for_each_entry_safe(waiting_lock,waiting_safe_lock,&wait_write_lh,list_node){
+     list_for_each_entry_safe(acquired_lock,acquired_safe_lock,&acquired_lh,list_node){
+       //not overlapped
+       if(waitingLockOverlapAcquired(waiting_lock,acquired_lock)==0){
+         waitingLockAdjustLink(waiting_lock);
+         addLockToAcquiredLock(waiting_lock);
+         //Finish procedure
+         return 0;
+       }
+     }
+   }
+
+   return 0;
+ };
 // int rl_acquire(void) {
 // return 0;
 // };
