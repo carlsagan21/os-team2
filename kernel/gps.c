7a11b877a0d827ac734c71d3e4d4e68165bc2a41
soo <carlsagan96@gmail.com>
2017-05-29 12:17:07 +0900

    gps.c scaffold

--- kernel/gps.c
+++ kernel/gps.c
@@ -1,4 +1,18 @@
+#include <linux/gps.h>
+
+// global gps
+struct gps_location gps;
+
 int set_gps_location(struct gps_location __user *loc)
 {
+	// kmalloc
+	// EPARAM 등
+	// value validation
+	// kfree
+	// Fractional parts for latitude and longitude should be nonnegative and do not exceed 999,999.
+	// The valid range for latitude is -90 and +90, and longitude is in the range of -180 and +180.
+	// accuracy should be nonnegative integer and the units of accuracy are in meter.
+	// From system boot to the first sys_gps_location system call, location is uninitialized.
+	// In this case, internal location information may have arbitrary value but it should meet the above criteria.
 	return 0;
 }
